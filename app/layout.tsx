637dc0b102f2f8b5be21518b565b1f390790017d
Dean Gans <deangans815@gmail.com>
2026-05-09 14:32:15 -0400

    fix: hamburger menu for mobile navbar, fix subtitle text wrapping on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,7 @@ export const metadata = {
 
 export const viewport = {
   width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
+  initialScale: 0.6,
 };
 
 export default function RootLayout({
